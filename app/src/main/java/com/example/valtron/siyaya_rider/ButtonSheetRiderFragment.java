4fd84c87d500606d9686d8d9dcfc0122dbee559c
Rome <meguel93@gmail.com>
2018-07-18 17:29:32 +0200

    Price estimation

--- app/src/main/java/com/example/valtron/siyaya_rider/ButtonSheetRiderFragment.java
+++ app/src/main/java/com/example/valtron/siyaya_rider/ButtonSheetRiderFragment.java
@@ -4,13 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.valtron.siyaya_rider.Common.Common;
+import com.example.valtron.siyaya_rider.Remote.IGoogleAPI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ButtonSheetRiderFragment extends BottomSheetDialogFragment {
     String mLocation, mDestination;
+
+    IGoogleAPI mService;
+
+    TextView txtMoney;
 
     public static ButtonSheetRiderFragment newInstance(String location, String destination)
     {
@@ -35,11 +51,67 @@ public class ButtonSheetRiderFragment extends BottomSheetDialogFragment {
         View view = inflater.inflate(R.layout.bottom_sheet_rider, container, false);
         TextView txtLocation = (TextView)view.findViewById(R.id.txtLocation);
         TextView txtDestination = (TextView)view.findViewById(R.id.txtDestination);
-        TextView txtMoney = (TextView)view.findViewById(R.id.txtMoney);
+        txtMoney = (TextView)view.findViewById(R.id.txtMoney);
+
+        mService = Common.getGoogleAPI();
+        getPrice(mLocation, mDestination);
 
         txtLocation.setText(mLocation);
         txtDestination.setText(mDestination);
         return view;
+    }
+
+    private void getPrice(String mLocation, String mDestination) {
+        String requestUrl = null;
+        try{
+            requestUrl = "https://maps.googleapis.com/maps/api/directions/json?" +
+                    "mode=driving&"
+                    +"transit_routing_preference=less_driving&"
+                    +"origin="+mLocation +"&"
+                    +"destination="+mDestination+"&"
+                    +"key="+getResources().getString(R.string.google_browser_key);
+            Log.e("LINK", requestUrl);
+            mService.getPath(requestUrl).enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+
+                    try {
+
+                        JSONObject jsonObject = new JSONObject(response.body().toString());
+                        JSONArray routes = jsonObject.getJSONArray("routes");
+
+                        JSONObject object = routes.getJSONObject(0);
+                        JSONArray legs = object.getJSONArray("legs");
+
+                        JSONObject legsObject = legs.getJSONObject(0);
+
+                        JSONObject distance = legsObject.getJSONObject("distance");
+                        String distance_text = distance.getString("text");
+                        Double distance_value = Double.parseDouble(distance_text.replaceAll("[^0-9\\\\.]+",""));
+
+                        JSONObject time = legsObject.getJSONObject("duration");
+                        String time_text = time.getString("text");
+                        Integer time_value = Integer.parseInt(time_text.replaceAll("\\D+",""));
+
+                        String final_calculation = String.format("%s + %s = R%.2f", distance_text, time_text,
+                                Common.getPrice(distance_value, time_value));
+
+                        txtMoney.setText(final_calculation);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Log.e("ERROR", t.getMessage());
+                }
+            });
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
 }
 
--- app/src/main/java/com/example/valtron/siyaya_rider/Common/Common.java
+++ app/src/main/java/com/example/valtron/siyaya_rider/Common/Common.java
@@ -1,7 +1,10 @@
 package com.example.valtron.siyaya_rider.Common;
 
 import com.example.valtron.siyaya_rider.Remote.FCMClient;
+import com.example.valtron.siyaya_rider.Remote.GoogleMapsAPI;
 import com.example.valtron.siyaya_rider.Remote.IFCMService;
+import com.example.valtron.siyaya_rider.Remote.IGoogleAPI;
+import com.example.valtron.siyaya_rider.Remote.RetrofitClient;
 
 public class Common {
 
@@ -12,10 +15,24 @@ public class Common {
     public static final String token_tbl = "Tokens";
 
     public static final String fcmURL = "https://fcm.googleapis.com/";
+    public static final String googleAPIURL = "https://maps.googleapis.com";
+
+    private static double base_fare = 2.55;
+    private static double time_rate = 2.55;
+    private static double distance_rate = 2.55;
+
+    public static double getPrice(double km, int min)
+    {
+        return (base_fare + (time_rate * min) + (distance_rate * km));
+    }
 
     public static IFCMService getFCMService()
     {
         return FCMClient.getClient(fcmURL).create(IFCMService.class);
     }
 
+    public static IGoogleAPI getGoogleAPI()
+    {
+        return GoogleMapsAPI.getClient(googleAPIURL).create(IGoogleAPI.class);
+    }
 }
